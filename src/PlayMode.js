0d96fe8c58c6903d84cbc2c15e57aa609065005a
Luke Turner <luke@lukedturner.com>
2023-11-29 10:17:37 +1100

    add random scale

--- src/PlayMode.js
+++ src/PlayMode.js
@@ -3,6 +3,9 @@ import Metronome from './Metronome';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './PlayMode.css';
 
+const musicalKeys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const scaleTypes = ["Major", "Minor"];
+
 const PlayMode = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -11,6 +14,10 @@ const PlayMode = () => {
   const [currentActivityIndex, setCurrentActivityIndex] = useState(0);
   const [timer, setTimer] = useState(null);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [currentScale, setCurrentScale] = useState("");
+  const [scaleFeatureEnabled, setScaleFeatureEnabled] = useState(false);
+  const [scaleChangeInterval, setScaleChangeInterval] = useState(60);
+  const [scaleTimer, setScaleTimer] = useState(60); // New state for scale timer
 
   useEffect(() => {
     if (currentActivityIndex < routine.length) {
@@ -35,6 +42,31 @@ const PlayMode = () => {
     return () => clearInterval(interval);
   }, [isTimerRunning, timer]);
 
+  useEffect(() => {
+    if (scaleFeatureEnabled && routine[currentActivityIndex].name === "Scales Practice") {
+      setCurrentScale(generateRandomScale());
+      setScaleTimer(scaleChangeInterval); // Reset scale timer
+
+      const scaleInterval = setInterval(() => {
+        setCurrentScale(generateRandomScale());
+        setScaleTimer(scaleChangeInterval); // Reset scale timer
+      }, scaleChangeInterval * 1000);
+
+      return () => clearInterval(scaleInterval);
+    }
+  }, [scaleFeatureEnabled, currentActivityIndex, routine, scaleChangeInterval]);
+
+  useEffect(() => {
+    let scaleTimerInterval;
+    if (scaleFeatureEnabled && scaleTimer > 0) {
+      scaleTimerInterval = setInterval(() => {
+        setScaleTimer(prevTimer => prevTimer - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(scaleTimerInterval);
+  }, [scaleTimer, scaleFeatureEnabled]);
+
   const toggleTimer = () => {
     setIsTimerRunning(!isTimerRunning);
   };
@@ -43,11 +75,16 @@ const PlayMode = () => {
     navigate('/');
   };
 
-  // Function to format time into minutes and seconds
   const formatTime = (totalSeconds) => {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
     return `${minutes}m ${seconds}s`;
+  };
+
+  const generateRandomScale = () => {
+    const randomKey = musicalKeys[Math.floor(Math.random() * musicalKeys.length)];
+    const randomType = scaleTypes[Math.floor(Math.random() * scaleTypes.length)];
+    return `${randomKey} ${randomType}`;
   };
 
   return (
@@ -56,12 +93,28 @@ const PlayMode = () => {
       {currentActivityIndex < routine.length ? (
         <>
           <p>Now Playing: {routine[currentActivityIndex].name} for {formatTime(timer)}</p>
-          {routine[currentActivityIndex].image && (
-            <img src={routine[currentActivityIndex].image} alt={routine[currentActivityIndex].name} style={{ maxWidth: '100%', height: 'auto' }} />
-          )}
           <button onClick={toggleTimer} className="play-mode-button">
             {isTimerRunning ? 'Pause' : 'Play'}
           </button>
+          {routine[currentActivityIndex].name === "Scales Practice" && (
+            <>
+              <button onClick={() => setScaleFeatureEnabled(!scaleFeatureEnabled)} className="play-mode-button">
+                {scaleFeatureEnabled ? "Disable Random Scales" : "Enable Random Scales"}
+              </button>
+              {scaleFeatureEnabled && (
+                <div>
+                  <p>Current Scale: {currentScale} - Time Remaining: {scaleTimer}s</p>
+                  <input
+                    type="number"
+                    value={scaleChangeInterval}
+                    onChange={(e) => setScaleChangeInterval(e.target.value)}
+                    className="scale-interval-input"
+                  />
+                  <span> seconds</span>
+                </div>
+              )}
+            </>
+          )}
         </>
       ) : (
         <p>Finished Routine!</p>
